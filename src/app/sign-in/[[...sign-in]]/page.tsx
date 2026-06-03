363ad2a998bffb450861f5a9e75704cd8dc556bb
shahmerrkhan <m.shahmeer.khan8@gmail.com>
2026-06-03 19:05:37 -0400

    fix duplicate sign-in property

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -64,7 +64,6 @@ export default function SignInPage() {
               },
               footerActionLink: { color: "#f5a623" },
               backLink: { color: "#aaa" },
-              identityPreviewEditButton: { color: "#f5a623" },
               formFieldAction: { color: "#f5a623" },
               navbar: { display: "none" },
               navbarMobileMenuButton: { display: "none" },
